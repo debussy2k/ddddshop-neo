178dc5eb6ec61fb8d6895806d52d5ce120868fe2
harry <harry@motion1.co.kr>
2025-09-08 11:02:20 +0900

    section-widget.svelte에서 Tailwind CSS 클래스의 표기법을 수정하여 빌드 호환성을 개선하고, 사용하지 않는 함수 주석 처리로 코드 가독성을 향상시킴.

--- src/lib/studio/widgets/section/section-widget.svelte
+++ src/lib/studio/widgets/section/section-widget.svelte
@@ -46,14 +46,16 @@
     }
 
 	function getInnerClass() {
-		let mobileClass = 'flex flex-col gap-y-2';
-		let desktopClass = prependSizeVariant('flex flex-row flex-wrap gap-x-2', '@3xl:'); // @3xl : 768px 이상
+        // tailwindcss에 정상 build되도록 fullname 표기 필요.
+		let mobileClass = 'flex flex-col gap-y-2';        
+        let desktopClass = "@3xl:flex @3xl:flex-row @3xl:flex-wrap @3xl:gap-x-2"; 
+		// let desktopClass = prependSizeVariant('flex flex-row flex-wrap gap-x-2', '@3xl:'); // @3xl : 768px 이상
 		return `w-full ${mobileClass} ${desktopClass}`;
 	}
 
-	function prependSizeVariant(classString: string, sizeVariant: string) {
-		return classString.split(/\s+/).map(cls => `${sizeVariant}${cls}`).join(' ');
-	}
+	// function prependSizeVariant(classString: string, sizeVariant: string) {
+	// 	return classString.split(/\s+/).map(cls => `${sizeVariant}${cls}`).join(' ');
+	// }
 
 	export function getContentHeight() {
 		if (!sectionElement) return 0;
