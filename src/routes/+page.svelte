18c0006decc0d49a856253db8d6cb0feb6e2784c
harry <harry@motion1.co.kr>
2025-09-16 11:56:28 +0900

    네비게이션 대화상자 추가 및 키보드 단축키 기능 구현: Ctrl+0으로 대화상자를 열고 ESC로 닫을 수 있도록 하여 사용자 경험을 개선했습니다. 또한, +layout.svelte 파일에 네비게이션 메뉴 항목을 추가했습니다.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import type { PageData } from './$types';
 	
 	interface Props {
@@ -11,14 +11,12 @@
 	onMount(async () => {
 
 	});
+	onDestroy(() => {
+
+	});
 
 </script>
 
-<div class='p-2 border-b border-gray-200 flex gap-4'>
-	<a href="/admin" class='text-sm text-gray-500'>admin</a>
-	<a href="/admin/studio" class='text-sm text-gray-500'>studio</a>
-	<a href="/admin/site-test" class='text-sm text-gray-500'>site-test</a>
-	<a href="/dnd-test" class='text-sm text-gray-500'>dnd-test</a>
+<div>
+	Home
 </div>
-
-
