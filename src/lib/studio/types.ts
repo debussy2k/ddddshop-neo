c5271797dbffb498617058489e5581fb5b1690ca
harry <harry@motion1.co.kr>
2025-09-19 18:42:56 +0900

    BaseWidgetProp 인터페이스 수정: top 속성을 추가하고, right 및 bottom 속성을 새로 정의하여 위젯의 위치 및 크기 속성을 개선하였습니다. 코드의 일관성을 높이고 가독성을 향상시켰습니다.

--- src/lib/studio/types.ts
+++ src/lib/studio/types.ts
@@ -19,9 +19,11 @@ export interface DocState {
 // 위젯의 기본 위치/크기 속성을 나타내는 공통 인터페이스
 export interface BaseWidgetProp {
     left: string;
-    top: string;
     width: string;
+    right: string;
+    top: string;
     height: string;
+    bottom: string;
     horzAlign: HorizontalAlign;
     vertAlign: VerticalAlign;
 }
