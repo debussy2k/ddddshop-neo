9ec478fff73889356f5cfb886c25ff1d33123291
debussy2k <harry@motion1.co.kr>
2025-09-22 23:28:00 +0900

    Enhance draggable and resizable configurations: Added 'bottom' and 'height' properties to the widget interface for improved layout control. Refactored the setupResizable function to utilize a new utility for calculating position updates based on drag events, streamlining the resizing logic and enhancing responsiveness.

--- src/lib/studio/widgets/common/wui.ts
+++ src/lib/studio/widgets/common/wui.ts
@@ -16,6 +16,8 @@ export namespace wui {
 		right: string;
         horzAlign: HorizontalAlign;
         top: string;
+		bottom: string;
+		height: string;
     }
     export interface DraggableConfig {
         id: string;
@@ -29,8 +31,6 @@ export namespace wui {
      * @param config 드래그 설정 객체
      */
     export function setupDraggable(config: DraggableConfig): void {
-        
-        // 기존 draggable 설정 제거 (중복 방지)
         interact(config.element).draggable({
             listeners: {
                 start: (event: DragEvent) => {
@@ -97,16 +97,16 @@ export namespace wui {
     // for Resizable
     // ------------------------------------------------------------
 
-    export interface CurrentDimensions {
-        width: string;
-        height: string;
-        left: string;
-        top: string;
-    }
+    // export interface CurrentDimensions {
+    //     width: string;
+    //     height: string;
+    //     left: string;
+    //     top: string;
+    // }
     export interface ResizableConfig {
         id: string;
         element: HTMLElement|SVGElement;
-        getCurrentProp: () => CurrentDimensions; // 현재 크기/위치 정보는 reactive하기 때문에 함수로 전달
+        getCurrentProp: () => CurrentPosition; // 현재 크기/위치 정보는 reactive하기 때문에 함수로 전달
         updateCallback: (id: string, dimensions: { width: string; height: string; left: string; top: string }) => void;
         minSize?: { width: number; height: number };
         edges?: { top?: boolean; left?: boolean; bottom?: boolean; right?: boolean };
@@ -117,8 +117,6 @@ export namespace wui {
      * @param config 리사이즈 설정 객체
      */
     export function setupResizable(config: ResizableConfig): void {
-        let rect = { width: 0, height: 0, left: 0, top: 0 };
-
         // 기존 resizable 설정 제거 (중복 방지)
         interact(config.element).resizable({
             edges: config.edges || { top: true, left: true, bottom: true, right: true },
@@ -130,28 +128,64 @@ export namespace wui {
             listeners: {
                 start: (event: ResizeEvent) => {
                     studioDoc.historyManager.setBatchMode();
-                    const currentProp = config.getCurrentProp();
-                    rect.left = util.getNumberPart(currentProp.left);
-                    rect.width = util.getNumberPart(currentProp.width);
-                    rect.top = util.getNumberPart(currentProp.top);
-                    rect.height = util.getNumberPart(currentProp.height);
+                    event.stopPropagation();
                 },
                 move: (event: ResizeEvent) => {
-                    rect.left += event.deltaRect?.left || 0;
-                    rect.width += event.deltaRect?.width || 0;
-                    rect.top += event.deltaRect?.top || 0;
-                    rect.height += event.deltaRect?.height || 0;
-                    config.updateCallback(config.id, {
-                        width: rect.width + 'px',
-                        height: rect.height + 'px',
-                        left: rect.left + 'px',
-                        top: rect.top + 'px'
-                    });
+					console.log('move', event.deltaRect);
+					let newPosition = getNewPosition(event);
+                    config.updateCallback(config.id, newPosition as CurrentPosition);
                 },
                 end: (event: ResizeEvent) => {
                     studioDoc.historyManager.commitBatch();
+                    event.stopPropagation();
                 }
             }
         });
+
+		function getNewPosition(event: ResizeEvent) {
+			const prop = config.getCurrentProp();
+			let horzPos: Partial<CurrentPosition>;
+			let vertPos: Partial<CurrentPosition>;
+
+			let deltaRect = event.deltaRect || { left: 0, width: 0, top: 0, height: 0, right: 0, bottom: 0 };
+
+
+			// horizontal
+			if (prop.horzAlign === 'left') {
+				horzPos = {
+					left: util.getNumberPart(prop.left) + deltaRect?.left + 'px',
+					width: util.getNumberPart(prop.width) + deltaRect?.width + 'px',
+					right: 'auto'
+				}
+			}
+			else if (prop.horzAlign === 'right') {
+				horzPos = {
+					right: util.getNumberPart(prop.right) - deltaRect?.right + 'px',
+					width: util.getNumberPart(prop.width) + deltaRect?.width + 'px',
+					left: 'auto'
+				}
+			}
+			else if (prop.horzAlign === 'both') {
+				horzPos = {
+					left: util.getNumberPart(prop.left) + deltaRect?.left + 'px',
+					right: util.getNumberPart(prop.right) - deltaRect?.right + 'px',
+					width: 'auto',
+				}
+			}
+			else {
+				horzPos = {}
+			}
+
+			// vertical
+			vertPos = {
+				top: util.getNumberPart(prop.top) + deltaRect?.top + 'px',
+				height: util.getNumberPart(prop.height) + deltaRect?.height + 'px',
+			}
+
+			return {
+				...horzPos,
+				...vertPos
+			}
+		}
     }
 }
