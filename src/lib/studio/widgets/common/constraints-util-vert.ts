3422f938cffab9dd29e5d973cceec952483ee8f5
harry <harry@motion1.co.kr>
2025-09-23 17:24:22 +0900

    위젯의 드래그 및 리사이즈 기능 개선: constraints-util-horz.ts 파일에서 수평 정렬 관련 함수들을 최적화하고, 드래그 및 리사이즈 기능에서의 일관성을 높이기 위해 해당 모듈을 업데이트했습니다. 이를 통해 위젯의 위치 조정 및 반응성을 더욱 향상시켰습니다.

---
+++ src/lib/studio/widgets/common/constraints-util-vert.ts
@@ -0,0 +1,145 @@
+import type { BaseWidgetProp, VerticalAlign } from "$lib/studio/types";
+import { util } from "$lib/studio/util";
+
+type VertProp = {
+	top: string;
+	bottom: string;
+	height: string;
+	centerOffsetY: number;
+	vertAlign: VerticalAlign;
+}
+
+
+export namespace constraintsUtilVert {
+	export function createVertAlignProps(newVertAlign: VerticalAlign, currentProp: BaseWidgetProp, parentHeight: number) {
+		let obj: Partial<BaseWidgetProp> = {};
+		if (newVertAlign === 'top') {
+			let top = getTopValuePx(currentProp, parentHeight);
+			obj = {
+				vertAlign: "top",
+				top: top,
+				height: getHeightValuePx(currentProp, parentHeight),
+				bottom: 'auto',
+				centerOffsetY: 0
+			}
+		}
+		else if (newVertAlign === 'bottom') {
+			let bottom = getBottomValuePx(currentProp, parentHeight);
+			obj = {
+				vertAlign: "bottom",
+				top: 'auto',
+				height: getHeightValuePx(currentProp, parentHeight),
+				bottom: bottom,
+				centerOffsetY: 0
+			}
+		}
+		else if (newVertAlign === 'both') {
+			let top = getTopValuePx(currentProp, parentHeight);
+			let bottom = getBottomValuePx(currentProp, parentHeight);
+			obj = {
+				vertAlign: "both",
+				top: top,
+				bottom: bottom,
+				height: 'auto',
+				centerOffsetY: 0
+			}
+		}
+		else if (newVertAlign === 'center') {
+			let centerOffsetY = getCenterOffsetY(currentProp, parentHeight);
+			let child_height_half = getHeightValue(currentProp, parentHeight)/2;
+			let top = `calc(50% + ${centerOffsetY}px - ${child_height_half}px)`;
+			obj = {
+				vertAlign: "center",
+				top: top,
+				height: getHeightValuePx(currentProp, parentHeight),
+				bottom: 'auto',
+				centerOffsetY: centerOffsetY,
+			}
+		}
+		else if (newVertAlign === 'scale') {
+			let topPercent = toPercent(getTopValue(currentProp, parentHeight), parentHeight);
+			let bottomPercent = toPercent(getBottomValue(currentProp, parentHeight), parentHeight);
+			obj = {
+				vertAlign: "scale",
+				top: topPercent + '%',
+				height: "auto",
+				bottom: bottomPercent + '%',
+				centerOffsetY: 0
+			}
+		}
+		else {
+			console.error(`invalid vert align`, newVertAlign);
+		}	
+
+		return obj;
+	}
+
+	export function getNumberPart(param: string) {
+		return Number(param.replace('px', ''));
+	}	
+	export function toPercent(val: number, parentHeight: number, decimal: number = 2) {
+		return Number(val/parentHeight*100).toFixed(decimal);
+	}	
+
+	export function getTopValue(prop: VertProp, parentHeight: number):number {
+		if (prop.vertAlign === 'center') {
+			let top = parentHeight/2 + prop.centerOffsetY - getNumberPart(prop.height)/2;
+			return top;
+		}
+		else if (prop.vertAlign === 'scale') {
+			let top = parentHeight * Number(prop.top.replace('%', '')) / 100;
+			return top;
+		}
+		else if (prop.top === 'auto') {
+			let top = parentHeight - (getNumberPart(prop.height) + getNumberPart(prop.bottom));
+			return top;
+		}
+		else {
+			return getNumberPart(prop.top);
+		}
+
+	}
+	export function getTopValuePx(prop: VertProp, parentHeight: number) {
+		return getTopValue(prop, parentHeight) + 'px';
+	}
+	
+	export function getBottomValue(prop: VertProp, parentHeight: number):number {
+		if (prop.vertAlign === 'center') {
+			let bottom = parentHeight/2 - prop.centerOffsetY - getNumberPart(prop.height)/2;
+			return bottom;
+		}
+		else if (prop.vertAlign === 'scale') {
+			let bottom = parentHeight * Number(prop.bottom.replace('%', '')) / 100;
+			return bottom;
+		}
+		else if (prop.bottom === 'auto') {
+			let bottom = parentHeight - (getNumberPart(prop.top) + getNumberPart(prop.height));
+			return bottom;
+		}
+		else {
+			return getNumberPart(prop.bottom);
+		}
+	}
+	export function getBottomValuePx(prop: VertProp, parentHeight: number) {
+		return getBottomValue(prop, parentHeight) + 'px';
+	}
+
+	export function getHeightValue(prop: VertProp, parentHeight: number):number {
+		if (prop.height === 'auto') {
+			return parentHeight - (getTopValue(prop, parentHeight) + getBottomValue(prop, parentHeight));
+		}
+		else {
+			return getNumberPart(prop.height);
+		}
+	}
+	export function getHeightValuePx(prop: VertProp, parentHeight: number) {
+		return getHeightValue(prop, parentHeight) + 'px';
+	}
+
+	export function getCenterOffsetY(prop: VertProp, parentHeight: number):number {
+		let top = getTopValue(prop, parentHeight);
+		let height = getHeightValue(prop, parentHeight);
+		let centerOffsetY =  (top + height/2) - parentHeight/2;
+		return centerOffsetY;
+	}	
+}
