48f6750e23d90b5a473b4203dad4eea569176183
harry <harry@motion1.co.kr>
2025-09-23 11:19:08 +0900

    수평 정렬 유틸리티 함수 통합: constraints-util.ts 파일에 HorzProp 타입을 추가하고, 수평 정렬 관련 함수들을 통합하여 위젯의 위치 조정 로직을 개선했습니다. util.ts에서 불필요한 함수들을 제거하고, constraintsUtil에서 직접 호출하도록 변경하여 코드의 일관성을 높였습니다.

--- src/lib/studio/util.ts
+++ src/lib/studio/util.ts
@@ -1,12 +1,5 @@
 import type { HorizontalAlign } from "./types";
 
-type HorzProp = {
-	left: string;
-	right: string;
-	width: string;
-	centerOffsetX: number;
-	horzAlign: HorizontalAlign;
-}
 
 export namespace util {
 	/**
@@ -17,44 +10,5 @@ export namespace util {
 
 	}	
 
-	export function getLeftValuePx(prop: HorzProp, parentWidth: number) {
-		if (prop.horzAlign === 'center') {
-			let left = parentWidth/2 + prop.centerOffsetX - getNumberPart(prop.width)/2;
-			return left + 'px';
-		}
-		else if (prop.left === 'auto') {
-			let left = parentWidth - (getNumberPart(prop.width) + getNumberPart(prop.right));
-			return left + 'px';
-		}
-		else {
-			return prop.left;
-		}
-	}
-	export function getRightValuePx(prop: HorzProp, parentWidth: number) {
-		if (prop.horzAlign === 'center') {
-			 let right = parentWidth/2 - prop.centerOffsetX - getNumberPart(prop.width)/2;
-			 return right + 'px';
-		}
-		else if (prop.right === 'auto') {
-			let right = parentWidth - (getNumberPart(prop.left) + getNumberPart(prop.width));
-			return right + 'px';
-		}
-		else {
-			return prop.right;
-		}
-	}
-	export function getWidthValuePx(prop: HorzProp, parentWidth: number) {
-		if (prop.width === 'auto') {
-			return parentWidth - (getNumberPart(prop.left) + getNumberPart(prop.right)) + 'px';
-		}
-		else {
-			return prop.width;
-		}
-	}
-	export function getCenterOffsetX(prop: HorzProp, parentWidth: number) {
-		let left = getNumberPart(getLeftValuePx(prop, parentWidth));
-		let width = getNumberPart(getWidthValuePx(prop, parentWidth));
-		let centerOffsetX =  (left + width/2) - parentWidth/2;
-		return centerOffsetX + 'px';
-	}
+
 }
--- src/lib/studio/widgets/common/constraints-util.ts
+++ src/lib/studio/widgets/common/constraints-util.ts
@@ -1,31 +1,39 @@
 import type { BaseWidgetProp, HorizontalAlign } from "$lib/studio/types";
 import { util } from "$lib/studio/util";
 
+type HorzProp = {
+	left: string;
+	right: string;
+	width: string;
+	centerOffsetX: number;
+	horzAlign: HorizontalAlign;
+}
+
 
 export namespace constraintsUtil {
 	export function createHorzAlignProps(newHorzAlign: HorizontalAlign, currentProp: BaseWidgetProp, parentWidth: number) {
 		let obj: Partial<BaseWidgetProp> = {};
 		if (newHorzAlign === 'left') {
-			let left = util.getLeftValuePx(currentProp, parentWidth);
+			let left = getLeftValuePx(currentProp, parentWidth);
 			obj = {
 				horzAlign: "left",
 				left: left,
-				width: util.getWidthValuePx(currentProp, parentWidth),
+				width: getWidthValuePx(currentProp, parentWidth),
 				right: 'auto',
 			}
 		}
 		else if (newHorzAlign === 'right') {
-			let right = util.getRightValuePx(currentProp, parentWidth);
+			let right = getRightValuePx(currentProp, parentWidth);
 			obj = {
 				horzAlign: "right",
 				left: 'auto',
-				width: util.getWidthValuePx(currentProp, parentWidth),
+				width: getWidthValuePx(currentProp, parentWidth),
 				right: right,
 			}
 		}
 		else if (newHorzAlign === 'both') {
-			let left = util.getLeftValuePx(currentProp, parentWidth);
-			let right = util.getRightValuePx(currentProp, parentWidth);
+			let left = getLeftValuePx(currentProp, parentWidth);
+			let right = getRightValuePx(currentProp, parentWidth);
 			obj = {
 				horzAlign: "both",
 				left: left,
@@ -34,13 +42,13 @@ export namespace constraintsUtil {
 			}
 		}
 		else if (newHorzAlign === 'center') {
-			let centerOffsetX = util.getCenterOffsetX(currentProp, parentWidth);
-			let child_width_half = util.getNumberPart(util.getWidthValuePx(currentProp, parentWidth))/2;
+			let centerOffsetX = getCenterOffsetX(currentProp, parentWidth);
+			let child_width_half = util.getNumberPart(getWidthValuePx(currentProp, parentWidth))/2;
 			let left = `calc(50% + ${centerOffsetX} - ${child_width_half}px)`;
 			obj = {
 				horzAlign: "center",
 				left: left,
-				width: util.getWidthValuePx(currentProp, parentWidth),
+				width: getWidthValuePx(currentProp, parentWidth),
 				right: 'auto',
 				centerOffsetX: util.getNumberPart(centerOffsetX),
 			}
@@ -51,4 +59,50 @@ export namespace constraintsUtil {
 
 		return obj;
 	}
+
+	export function getNumberPart(param: string) {
+		return parseInt(param.replace('px', ''));
+
+	}	
+
+	export function getLeftValuePx(prop: HorzProp, parentWidth: number) {
+		if (prop.horzAlign === 'center') {
+			let left = parentWidth/2 + prop.centerOffsetX - getNumberPart(prop.width)/2;
+			return left + 'px';
+		}
+		else if (prop.left === 'auto') {
+			let left = parentWidth - (getNumberPart(prop.width) + getNumberPart(prop.right));
+			return left + 'px';
+		}
+		else {
+			return prop.left;
+		}
+	}
+	export function getRightValuePx(prop: HorzProp, parentWidth: number) {
+		if (prop.horzAlign === 'center') {
+			 let right = parentWidth/2 - prop.centerOffsetX - getNumberPart(prop.width)/2;
+			 return right + 'px';
+		}
+		else if (prop.right === 'auto') {
+			let right = parentWidth - (getNumberPart(prop.left) + getNumberPart(prop.width));
+			return right + 'px';
+		}
+		else {
+			return prop.right;
+		}
+	}
+	export function getWidthValuePx(prop: HorzProp, parentWidth: number) {
+		if (prop.width === 'auto') {
+			return parentWidth - (getNumberPart(prop.left) + getNumberPart(prop.right)) + 'px';
+		}
+		else {
+			return prop.width;
+		}
+	}
+	export function getCenterOffsetX(prop: HorzProp, parentWidth: number) {
+		let left = getNumberPart(getLeftValuePx(prop, parentWidth));
+		let width = getNumberPart(getWidthValuePx(prop, parentWidth));
+		let centerOffsetX =  (left + width/2) - parentWidth/2;
+		return centerOffsetX + 'px';
+	}	
 }
