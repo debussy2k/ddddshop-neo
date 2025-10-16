c832560aa3203cc8ac6c7c181758868f54e6bfa1
harry <harry@motion1.co.kr>
2025-10-16 17:30:09 +0900

    추가: PageWidget 컴포넌트를 infinite-canvas-view에 통합하여 섹션 렌더링 방식 개선
    - infinite-canvas-view.svelte 파일에 PageWidget을 추가하여 문서의 섹션을 보다 효율적으로 렌더링하도록 수정했습니다.
    - 기존의 데스크탑 및 모바일 뷰에서 섹션 렌더링 코드를 제거하여 코드 간결성을 높였습니다.

---
+++ src/lib/studio/widgets/section/page-widget.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { DocState } from "../../types";
+    import SectionWidget from "./section-widget.svelte";
+
+    let { doc }: { doc: DocState } = $props();
+
+</script>
+
+
+<!-- desktop -->
+<div class='page @container absolute shadow-lg shadow-gray-400 mt-4 ' style="width:992px;"
+>
+    {#each doc.sections as section (section.id)}
+        <SectionWidget data={section} />
+    {/each}
+</div>
+
+<!-- mobile -->
+<div class='page @container absolute shadow-lg shadow-gray-400 mt-4 ' style="left:1024px; width:480px;">
+    {#each doc.sections as section (section.id)}
+        <SectionWidget data={section} />
+    {/each}
+</div>
