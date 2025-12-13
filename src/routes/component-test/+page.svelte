a142ef46bbcf0e687e34bddbb180700b3583a790
debussy2k <harry@motion1.co.kr>
2025-12-14 01:15:38 +0900

    Refactor component test to use snippet for beta implementation rendering

--- src/routes/component-test/+page.svelte
+++ src/routes/component-test/+page.svelte
@@ -11,12 +11,17 @@
 	Component Test
 </div>
 
+<!-- 실제 구현은 이런 식으로 따로 구현 -->
+{#snippet impl_beta(spec:string)}
+	<p>Beta : {spec}</p>
+{/snippet}
+
 <LayoutComp>
 	{#snippet impl(id: string, spec:string)}
 		{#if id === 'alpha'}
 			<p>Alpha : {spec}</p>
 		{:else if id === 'beta'}
-			<p>Beta : {spec}</p>
+			{@render impl_beta(spec)}
 		{:else}
 			<p>Unknown : {spec}</p>
 		{/if}
