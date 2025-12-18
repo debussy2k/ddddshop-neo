d72a03feadc39c927f809358640b45fb662756a9
harry <harry@motion1.co.kr>
2025-12-19 00:28:57 +0900

    Enhance About and Home components with styling and navigation link. The About component now includes a styled container, and the Home component features a link to the About page.

--- src/routes/site/[tenant_site]/[...path]/pageComponent/tville-ddddshop/about/index.svelte
+++ src/routes/site/[tenant_site]/[...path]/pageComponent/tville-ddddshop/about/index.svelte
@@ -1,1 +1,3 @@
-<div>About Component</div>
+<div class='w-[300px] h-[200px] border border-gray-300 flex items-center justify-center'>
+    About Component
+</div>
--- src/routes/site/[tenant_site]/[...path]/pageComponent/tville-ddddshop/home/index.svelte
+++ src/routes/site/[tenant_site]/[...path]/pageComponent/tville-ddddshop/home/index.svelte
@@ -1,1 +1,5 @@
-<div>Home Component</div>
+<div class='w-[300px] h-[200px] border border-gray-300 p-4'>
+    <div>Home Component</div>
+    <div>go to <a href='./about'>about</a></div>
+    
+</div>
